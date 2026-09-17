95549523b1ebceccb56034e473c43c26ac8dbc9b
LuisAntonioJu <luisantoniodeassisjunior@gmail.com>
2026-09-17 17:24:08 -0300

    Entregado

--- lib/lista_contatos_page.dart
+++ lib/lista_contatos_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'database_helper.dart';
+import 'package:app_contatos/sobre_app_page.dart';
 
 class ListaContatosPage extends StatefulWidget {
   const ListaContatosPage({super.key});
@@ -81,6 +82,46 @@ class _ListaContatosPageState extends State<ListaContatosPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Lista de Contatos"), centerTitle: true),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(color: Colors.indigo),
+              child: Text(
+                "Meus Contatos",
+                style: TextStyle(color: Colors.white, fontSize: 22),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.list),
+              title: Text("Todas os Contatos"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.check_circle),
+              title: Text("Concluídas"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.circle_outlined),
+              title: Text("Pendentes"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.info_outline),
+              title: Text("Sobre o App"),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => SobreAppPage()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
       body: contatos.isEmpty
           ? Center(
               child: Text('Nenhum contato ainda. Toque em + para adicionar.'),

17cbe37ad2331803947830785910378101d24263
LuisAntonioJu <luisantoniodeassisjunior@gmail.com>
2026-09-18 14:37:46 -0300

    FaseEF

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -34,9 +34,7 @@ class DatabaseHelper {
   // READ: Buscar todos os contatos salvos no banco
   static Future<List<Map<String, dynamic>>> buscarContatos() async {
     final db = await DatabaseHelper.database;
-    return await db.query(
-      'contatos',
-    ); // SELECT * FROM contatos (já traz a sigla pronta)
+    return await db.query('contatos');
   }
 
   // CREATE: Inserir um novo contato no banco salvando titulo, numero e sigla
@@ -51,8 +49,46 @@ class DatabaseHelper {
     await db.insert('contatos', {
       'titulo': titulo,
       'numero': numero,
-      'sigla': sigla, // Salva a sigla aqui
+      'sigla': sigla,
       'situacao': 0,
     });
+  }
+
+  // UPDATE: Acionar contato
+  static Future<void> atualizarSituacao(int id, int novaSituacao) async {
+    final db = await DatabaseHelper.database;
+    await db.update(
+      'contatos',
+      {'situacao': novaSituacao},
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
+  // UPDATE: Edição de número e nome
+  static Future<void> editarContato(
+    int id,
+    String titulo,
+    String numero,
+  ) async {
+    final db = await DatabaseHelper.database;
+
+    String sigla = '?';
+    if (titulo.isNotEmpty) {
+      sigla = titulo[0].toUpperCase();
+    }
+
+    await db.update(
+      'contatos',
+      {'titulo': titulo, 'numero': numero, 'sigla': sigla},
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
+  // DELETE: Apagar contato do banco
+  static Future<void> excluirContato(int id) async {
+    final db = await DatabaseHelper.database;
+    await db.delete('contatos', where: 'id = ?', whereArgs: [id]);
   }
 }
--- lib/lista_contatos_page.dart
+++ lib/lista_contatos_page.dart
@@ -25,6 +25,12 @@ class _ListaContatosPageState extends State<ListaContatosPage> {
     });
   }
 
+  void alternarEstrela(int id, int situacaoAtual) async {
+    int novaSituacao = situacaoAtual == 1 ? 0 : 1;
+    await DatabaseHelper.atualizarSituacao(id, novaSituacao);
+    carregarContatos();
+  }
+
   void adicionarContato() {
     final nomeController = TextEditingController();
     final numeroController = TextEditingController();
@@ -58,7 +64,6 @@ class _ListaContatosPageState extends State<ListaContatosPage> {
             TextButton(
               onPressed: () async {
                 if (nomeController.text.isNotEmpty) {
-                  // Salva o nome e o número separados
                   await DatabaseHelper.inserirContato(
                     nomeController.text,
                     numeroController.text,
@@ -71,6 +76,59 @@ class _ListaContatosPageState extends State<ListaContatosPage> {
                 }
               },
               child: Text('Adicionar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void editarContato(Map<String, dynamic> contato) {
+    final nomeController = TextEditingController(text: contato['titulo']);
+    final numeroController = TextEditingController(text: contato['numero']);
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Editar Contato'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: nomeController,
+                decoration: InputDecoration(hintText: 'Digite o nome...'),
+              ),
+              TextField(
+                controller: numeroController,
+                keyboardType: TextInputType.phone,
+                decoration: InputDecoration(hintText: 'Digite o número...'),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Cancelar'),
+            ),
+            TextButton(
+              onPressed: () async {
+                if (nomeController.text.isNotEmpty) {
+                  await DatabaseHelper.editarContato(
+                    contato['id'],
+                    nomeController.text,
+                    numeroController.text,
+                  );
+                  carregarContatos();
+
+                  if (!context.mounted) return;
+
+                  Navigator.pop(context);
+                }
+              },
+              child: Text('Salvar'),
             ),
           ],
         );
@@ -94,18 +152,18 @@ class _ListaContatosPageState extends State<ListaContatosPage> {
               ),
             ),
             ListTile(
-              leading: Icon(Icons.list),
+              leading: Icon(Icons.contacts),
               title: Text("Todas os Contatos"),
               onTap: () {},
             ),
             ListTile(
-              leading: Icon(Icons.check_circle),
-              title: Text("Concluídas"),
+              leading: Icon(Icons.star),
+              title: Text("Contatos Favoritos"),
               onTap: () {},
             ),
             ListTile(
-              leading: Icon(Icons.circle_outlined),
-              title: Text("Pendentes"),
+              leading: Icon(Icons.star_border),
+              title: Text("Contatos Não Favoritos"),
               onTap: () {},
             ),
             ListTile(
@@ -131,18 +189,51 @@ class _ListaContatosPageState extends State<ListaContatosPage> {
               itemCount: contatos.length,
               itemBuilder: (context, index) {
                 final contato = contatos[index];
+                bool estaFavorito = contato['situacao'] == 1;
 
-                return Card(
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      backgroundColor: Colors.blue,
-                      child: Text(
-                        contato['sigla'] ?? '?',
-                        style: TextStyle(color: Colors.white),
+                return Dismissible(
+                  key: Key(contato['id'].toString()),
+                  direction: DismissDirection.endToStart,
+                  background: Container(
+                    color: Colors.red,
+                    alignment: Alignment.centerRight,
+                    padding: EdgeInsets.only(right: 20),
+                    child: Icon(
+                      Icons.delete,
+                      color: Colors.white,
+                    ),
+                  ),
+                  onDismissed: (direction) async {
+                    await DatabaseHelper.excluirContato(contato['id']);
+                    carregarContatos();
+                  },
+                  child: Card(
+                    child: ListTile(
+                      onTap: () {
+                        editarContato(contato);
+                      },
+                      leading: CircleAvatar(
+                        backgroundColor: Colors.blue,
+                        child: Text(
+                          contato['sigla'] ?? '?',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      title: Text(contato['titulo'] ?? ''),
+                      subtitle: Text(contato['numero'] ?? ''),
+                      trailing: IconButton(
+                        icon: Icon(
+                          Icons.star,
+                          color: estaFavorito ? Colors.orange : Colors.grey,
+                        ),
+                        onPressed: () {
+                          alternarEstrela(
+                            contato['id'],
+                            contato['situacao'] ?? 0,
+                          );
+                        },
                       ),
                     ),
-                    title: Text(contato['titulo'] ?? ''),
-                    subtitle: Text(contato['numero'] ?? ''),
                   ),
                 );
               },
